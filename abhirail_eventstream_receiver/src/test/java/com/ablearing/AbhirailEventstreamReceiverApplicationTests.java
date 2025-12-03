3ae12a51f0c945dadfc993546b64401a4e66756a
Abhilash Rupa <abhilashrupa@gmail.com>
2025-12-03 16:39:58 +0530

    Squashed 'kafka-demos/' content from commit 281c587
    git-subtree-dir: kafka-demos
    git-subtree-split: 281c587634d8ba24020a50a3692cdc1b8d730eae

---
+++ abhirail_eventstream_receiver/src/test/java/com/ablearing/AbhirailEventstreamReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ablearing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbhirailEventstreamReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ abhirail_eventstream_sender/src/test/java/com/ablearning/AbhirailEventstreamSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ablearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbhirailEventstreamSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
